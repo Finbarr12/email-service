e3bdcfcaf2a9a82a24b7f215223f6700b6da6297
finbarr12 <finarrogbonna@gmail.com>
2023-04-12 12:42:27 +0100

    serverrunning

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express"
+
+const app:Application = express()
+const Port = 1212
+
+app.use(express.json())
+
+
+app.listen(Port,()=>{
+    console.log("Up and running")
+})
