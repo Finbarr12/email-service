823872703c834fb26e547c954a09c91f02e12007
finbarr12 <finarrogbonna@gmail.com>
2023-04-12 13:19:19 +0100

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
+import router from "../router/router";
 
 const app: Application = express();
 const Port = 1212;
 
 app.use(express.json());
+
+app.use("/api", router);
 
 app.listen(Port, () => {
   console.log("Up and running");
