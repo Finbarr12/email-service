1aa2c64b4fe318f9f292c5932850abcec24596f2
finbarr12 <finarrogbonna@gmail.com>
2023-04-12 12:52:52 +0100

    errormssg

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import express, { Application } from "express";
-import nodemailer from "nodemailer";
-import sendInBlueTransport from "nodemailer-sendinblue-transport";
 
 const app: Application = express();
 const Port = 1212;
